e52b1cd1f7c8625105d90b44c548bac6517a90d2
Tom Quinn <tom.quinn@teeque.co.uk>
2023-07-13 22:42:30 +0100

    Two Sum

---
+++ Two_Sum.c
@@ -0,0 +1,14 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int* ret = (int*) malloc(2 * sizeof(int));
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++){
+        for (int j = i + 1; j < numsSize; j++){
+            if (nums[i] + nums[j] == target){
+                ret[0] = i;
+                ret[1] = j;
+                return ret;
+            }
+        }
+    }
+    return ret;
+}
